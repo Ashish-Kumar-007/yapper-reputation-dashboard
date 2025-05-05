8568586822abf13afe781e79e90af3717ed3f295
Ashish Kumar Sahoo <ashishsahoo2015@gmail.com>
2025-05-06 01:08:43 +0530

    Modified Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -6,14 +5,16 @@ const cors = require('cors');
 const yapperRoutes = require('./routes/yapperRoutes');
 
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/yapper', yapperRoutes);
 
+// Connect to MongoDB when app is first used (lazy connection pattern)
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(5000, () => console.log('Server running on http://localhost:5000'));
-  })
-  .catch(err => console.log(err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = app; // Export app for Vercel
